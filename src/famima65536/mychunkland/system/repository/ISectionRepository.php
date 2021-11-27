6fea0462851bc8f9f187aff7d194f137c4e2a6a9
famima65536 <famima65536@gmail.com>
2021-11-27 11:27:12 +0900

    add Section Repository interface and InMemorySectionRepository

---
+++ src/famima65536/mychunkland/system/repository/ISectionRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace famima65536\mychunkland\system\repository;
+
+use famima65536\mychunkland\system\model\ChunkCoordinate;
+use famima65536\mychunkland\system\model\Section;
+use famima65536\mychunkland\system\model\User;
+use famima65536\mychunkland\system\model\UserId;
+
+interface ISectionRepository {
+	/**
+	 * @param ChunkCoordinate $coordinate
+	 * @return Section|null
+	 */
+	public function findByCoordinate(ChunkCoordinate $coordinate): ?Section;
+
+	/**
+	 * @param Section $section
+	 */
+	public function save(Section $section): void;
+
+	/**
+	 * @param UserId $userId
+	 * @return Section[]
+	 */
+	public function findByOwner(UserId $userId): array;
+
+	/**
+	 * @param UserId $userId
+	 * @return Section[]
+	 */
+	public function findByShared(UserId $userId): array;
+
+}
---
+++ src/famima65536/mychunkland/system/repository/InMemorySectionRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace famima65536\mychunkland\system\repository;
+
+use famima65536\mychunkland\system\model\ChunkCoordinate;
+use famima65536\mychunkland\system\model\Section;
+use famima65536\mychunkland\system\model\UserId;
+use pocketmine\level\Level;
+
+/**
+ * for test, In memory Section Repository
+ */
+class InMemorySectionRepository implements ISectionRepository {
+
+	/**
+	 * @var array<string, array<int, Section>>
+	 */
+	private array $sections = [];
+
+	public function __construct(){
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function findByCoordinate(ChunkCoordinate $coordinate): ?Section{
+		if(!isset($this->sections[$coordinate->getWorldName()])){
+			return null;
+		}
+
+		return clone $this->sections[$coordinate->getWorldName()][Level::chunkHash($coordinate->getX(), $coordinate->getZ())] ?? null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function save(Section $section): void{
+		$coordinate = $section->getCoordinate();
+		$this->sections[$coordinate->getWorldName()][Level::chunkHash($coordinate->getX(), $coordinate->getZ())] = $section;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function findByOwner(UserId $userId): array{
+		$result = [];
+		foreach($this->sections as $sectionsByWorld){
+			foreach($sectionsByWorld as $section){
+				if($section->getOwnerId()->equals($userId)){
+					$result[] = clone $section;
+				}
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function findByShared(UserId $userId): array{
+		$result = [];
+		foreach($this->sections as $sectionsByWorld){
+			foreach($sectionsByWorld as $section){
+				if($section->getShareGroup()->contains($userId)){
+					$result[] = clone $section;
+				}
+			}
+		}
+		return $result;
+	}
+}
