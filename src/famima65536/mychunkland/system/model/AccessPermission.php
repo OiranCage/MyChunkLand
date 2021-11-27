72a02b6283db30d78bdc57db7c89642620bfd6a5
famima65536 <famima65536@gmail.com>
2021-11-27 10:17:29 +0900

    add read write and execute permission to AccessPermission

--- src/famima65536/mychunkland/system/model/AccessPermission.php
+++ src/famima65536/mychunkland/system/model/AccessPermission.php
@@ -2,6 +2,43 @@
 
 namespace famima65536\mychunkland\system\model;
 
+/**
+ * Immutable value-type class
+ */
 class AccessPermission {
+	public function __construct(
+		private bool $readable,
+		private bool $writable,
+		private bool $executable
+	){
+	}
 
+	/**
+	 * @return bool
+	 */
+	public function isReadable(): bool{
+		return $this->readable;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isWritable(): bool{
+		return $this->writable;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isExecutable(): bool{
+		return $this->executable;
+	}
+
+	public function isReadonly(): bool{
+		return (
+			$this->readable &&
+			!$this->writable &&
+			!$this->executable
+		);
+	}
 }
