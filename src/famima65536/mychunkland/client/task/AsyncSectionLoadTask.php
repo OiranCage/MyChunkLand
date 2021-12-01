0feb3082f300b54fa7050d355811a4979f233505
famima65536 <famima65536@gmail.com>
2021-12-01 14:27:13 +0900

    now callback is available in AsyncSectionLoadTask

--- src/famima65536/mychunkland/client/task/AsyncSectionLoadTask.php
+++ src/famima65536/mychunkland/client/task/AsyncSectionLoadTask.php
@@ -2,6 +2,7 @@
 
 namespace famima65536\mychunkland\client\task;
 
+use Closure;
 use famima65536\mychunkland\client\Loader;
 use famima65536\mychunkland\system\model\ChunkCoordinate;
 use famima65536\mychunkland\system\model\Section;
@@ -17,7 +18,7 @@ class AsyncSectionLoadTask extends AsyncTask {
 	 * @param ChunkCoordinate[] $chunkCoordinates
 	 * @param array $connectionConfig
 	 */
-	public function __construct(array $chunkCoordinates, private array $connectionConfig){
+	public function __construct(array $chunkCoordinates, private array $connectionConfig, private ?Closure $callback=null){
 		$this->serializedChunkCoordinates = serialize($chunkCoordinates);
 	}
 
@@ -39,6 +40,10 @@ class AsyncSectionLoadTask extends AsyncTask {
 		$loader = Loader::getInstance();
 		foreach($sections as $section){
 			$loader->cacheSection($section[0], $section[1]);
+		}
+
+		if($this->callback !== null){
+			($this->callback)($sections);
 		}
 	}
 }
